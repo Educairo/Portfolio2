d2560419a7bdd84479743608e349e324ef726a5c
Educairo <102773100+Educairo@users.noreply.github.com>
2024-11-30 23:38:13 -0300

    alteração api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Educairo/Portfolio/refs/heads/main/Portfolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Educairo/Portfolio2/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
